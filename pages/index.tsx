40fd6b9db80df12afd3d1f82c81743c533b57ee5
Dima <dimashikalov86@mail.ru>
2023-08-11 13:09:59 +0300

    added layout comp's

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,46 @@
-import { Button, Htag, P, Rating, Tag } from '../components';
-import { useState } from 'react';
+import { withLayout } from "@/layout/Layout";
+import { Button, Htag, P, Rating, Tag } from "../components";
+import React, { useState } from "react";
 
-
-export default function Home(): JSX.Element {
-  const [ rating, setRating ] = useState<number>(4);
+function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(4);
 
   return (
     <>
-      <Htag tag='h1'>Hello world</Htag>
-      <Button appearance='primary' arrow='right'>CLick me</Button>
-      <Button appearance='ghost' arrow='down' onClick={()=>console.log('click')}>CLick me</Button>
-      <P >Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, cupiditate.</P>
-      <P size='s'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, cupiditate.</P>
-      <P size="l">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, cupiditate.</P>
-      <Tag size='s'>Small</Tag>
-      <Tag size='m' color="red">mediem</Tag>
-      <Tag size='m' color="primary">mediem</Tag>
-      <Rating rating={ rating } isEditable setRating={ setRating } />
-   </>
+      <Htag tag="h1">Hello world</Htag>
+      <Button appearance="primary" arrow="right">
+        CLick me
+      </Button>
+      <Button
+        appearance="ghost"
+        arrow="down"
+        onClick={() => console.log("click")}
+      >
+        CLick me
+      </Button>
+      <P>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+        cupiditate.
+      </P>
+      <P size="s">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+        cupiditate.
+      </P>
+      <P size="l">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
+        cupiditate.
+      </P>
+      <Tag size="s">Small</Tag>
+      <Tag size="s">Small</Tag>
+      <Tag size="m" color="red">
+        mediem
+      </Tag>
+      <Tag size="m" color="primary">
+        mediem
+      </Tag>
+      <Rating rating={rating} isEditable setRating={setRating} />
+    </>
   );
 }
+
+export default withLayout(Home);
